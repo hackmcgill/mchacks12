d7812e819b853acfb99a7daa9778812189d479ee
bill_skills <billskills01@gmail.com>
2024-11-28 21:08:09 -0500

    More gradient tweaks (i know this is bad practice but i dont have spreadsheet ID to work on styling properly)

--- src/components/Logos/Content.jsx
+++ src/components/Logos/Content.jsx
@@ -8,10 +8,10 @@ export const Content = styled.section`
   gap: 10px;
 
   h1 {
-    font-size: 5rem;
+    font-size: 8rem;
     text-align: center;
-    color: ${styleVars.hackBlack};
-    margin: 0;
+    color: ${styleVars.m12DarkestTeal};
+    margin: 5rem 0;
     margin-bottom: 5rem;
     font-family: "ArcadeClassic", sans-serif;
   }
